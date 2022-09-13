6b7cf16d5fdde0a9b8aabac385a993a6e0300c40
Salman Ahmed <shahsalmanahmed009@gmail.com>
2022-09-13 23:23:08 +0600

    react-top-loading-bar npm package added. it show a loading bar on top

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,36 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import LoadingBar from "react-top-loading-bar";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const [progress, setProgress] = useState(0);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      setProgress(20);
+    });
+    router.events.on("routeChangeComplete", () => {
+      setProgress(100);
+    });
+  }, [router.events]);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <div>
+        <LoadingBar
+          color="blue"
+          progress={progress}
+          onLoaderFinished={() => setProgress(0)}
+        />
+      </div>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
